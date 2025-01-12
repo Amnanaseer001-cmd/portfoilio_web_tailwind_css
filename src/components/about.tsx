991b8cfed85b6917fa0fbaa749a8deacc80ff442
Amnanaseer001-cmd <amnanaseer924@gmail.com>
2025-01-12 02:19:12 +0000

    Updated image optimization and fixed alt text

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -18,9 +18,8 @@ export default function About() {
           <Image
             src="/main-img3.jpg"
             alt="Description of the image"
-            layout="responsive"
-            width={420}
-            height={470}
+            width={400}
+            height={700}
             className="ml-20 mt-10 rounded-lg border-[1px] border-gray-400 p-1 transition-transform duration-100 hover:scale-110"
           />
         </div>
@@ -33,12 +32,12 @@ export default function About() {
             <br /> Designer and Web Developer
           </p>
           <p className="mt-4 leading-8 text-[16px] text-gray-300">
-            I&apos;m Amna Naseer, a web developer skilled in HTML, CSS, JavaScript{" "}
-            <br /> and TypeScript.I'm currently expanding my expertise by
-            learning Next.js <br /> and building dynamic project Pursuing a
-            Bachelor in Computer Science <br /> I have hands-on experience with
-            C++ for software development. <br /> I enjoy creating engaging web
-            experiences and am passionate about <br />
+            I&apos;m Amna Naseer, a web developer skilled in HTML, CSS,
+            JavaScript <br /> and TypeScript.I'm currently expanding my
+            expertise by learning Next.js <br /> and building dynamic project
+            Pursuing a Bachelor in Computer Science <br /> I have hands-on
+            experience with C++ for software development. <br /> I enjoy
+            creating engaging web experiences and am passionate about <br />
             using modern technologies to bring ideas to life. <br />
           </p>
 
